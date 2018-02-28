a58747c7dff1c31fa28f3e2a19818d17ed0d0820
mihinsumaria <mihin.sumaria@gmail.com>
2018-02-27 23:59:21 -0500

    Added GET endpoint to display datasets

--- genex-interface/app/routes.py
+++ genex-interface/app/routes.py
@@ -1,7 +1,20 @@
 from app import app
-from flask import render_template
+from flask import render_template, jsonify, request,url_for
+import json
+
 
 @app.route('/')
 @app.route('/index')
 def index():
 	return render_template('index.html')
+
+@app.route('/datasets')
+def datasets():
+	with open('datasets.json','r') as datasets_json:
+		datasets = json.load(datasets_json)
+	keys = ['ID','name']
+	datasets = [{k: x[k] for k in keys} for x in datasets]
+	datasets_json.close()
+	return jsonify(datasets)
+
+
