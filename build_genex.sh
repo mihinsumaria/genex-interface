72b55406e2b0bcb0ff9e194c1873889e9e23239f
Cuong Nguyen <ctring23@gmail.com>
2018-03-17 19:29:39 -0400

    Update genex. Only build pygenex with build_genex.sh

--- build_genex.sh
+++ build_genex.sh
@@ -4,11 +4,10 @@ set -e
 
 echo "=============== Build GENEX ==============="
 cd genex
-mkdir -p build && cd build
-cmake ..
-make -j
+./build.sh --cmake "-DBUILD_PYGENEX=ON -DBUILD_CLI=OFF -DBUILD_TESTS=OFF"
 
+cd $ROOT
 mkdir -p $ROOT/webapp
-cp -v pygenex.so $ROOT/genex-interface/pygenex.so
+cp -v $ROOT/genex/build/pygenex.so $ROOT/genex-interface/pygenex.so
 
 echo "=============== DONE ==============="
