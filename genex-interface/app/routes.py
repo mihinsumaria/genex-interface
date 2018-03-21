7bc4e7f2b991cc0fe4b8888a9919a47a01da6041
mihir <mihir9552@gmail.com>
2018-03-20 21:19:22 -0400

    added base64 image

--- genex-interface/app/routes.py
+++ genex-interface/app/routes.py
@@ -2,6 +2,18 @@ from app import app
 from flask import render_template, jsonify, request, url_for
 import json
 import pygenex
+import matplotlib.pyplot as plt
+import numpy as np
+from io import BytesIO
+import base64
+
+html = '''
+<html>
+    <body>
+        <img src="data:image/png;base64, {}" />
+    </body>
+</html>
+'''
 
 @app.route('/')
 @app.route('/index')
@@ -23,5 +35,14 @@ def get_distances():
 	alldistances = [x for x in alldistances if 'dtw' not in x]
 	return jsonify(alldistances)
 
-
-
+@app.route('/test_base64')
+def get_base64_thumb():
+	data_name = pygenex.loadDataset('ItalyPowerDemand', 'datasets/test/ItalyPowerDemand_DATA')
+	data = pygenex.getTimeSeries(data_name['name'], 1, 3, 10)
+	fig = plt.figure(figsize=(15, 3))
+	ax = fig.add_subplot(1, 1, 1)
+	ax.plot(data)
+	io = BytesIO()
+	fig.savefig(io, format='png')
+	data = base64.encodestring(io.getvalue())
+	return html.format(data.decode())
