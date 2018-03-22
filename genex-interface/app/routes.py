7d4a4bd895d5eb7ba829a1f2f2167561b349fc2f
mihinsumaria <mihin.sumaria@gmail.com>
2018-03-21 20:06:50 -0400

    Added POST /preprocess

--- genex-interface/app/routes.py
+++ genex-interface/app/routes.py
@@ -34,3 +34,30 @@ def get_distances():
 	alldistances = pygenex.getAllDistances()
 	alldistances = [x for x in alldistances if 'dtw' not in x]
 	return jsonify(alldistances)
+
+preprocessed = {}
+
+@app.route('/preprocess', methods = ['GET','POST'])
+def preprocess_data():
+	if request.method == "POST":
+		dataset = int(request.form['dataset'])
+		st = float(request.form['st'])
+		distance = str(request.form['distance'])
+
+		key = str(dataset) + distance + str(st)
+		if key in preprocessed:
+			return jsonify(preprocessed[key])	
+
+		else:
+			
+			with open('datasets.json','r') as datasets_json:
+				datasets = json.load(datasets_json)
+
+			name = str(datasets[dataset-1]['name'])
+			path = str(datasets[dataset-1]['path'])
+			loaddetails = pygenex.loadDataset(name,path)
+			groupdetails = pygenex.group(name,st,distance)
+			subsequences = loaddetails['count']*loaddetails['length']*(loaddetails['length']-1)/2
+
+			preprocessed[key] = {'ID':dataset,'name':name,'st':st,'distance':distance,'count':loaddetails['count'], 'length':loaddetails['length'],'subsequences':subsequences,'groups':groupdetails}
+			return jsonify(preprocessed[key])
