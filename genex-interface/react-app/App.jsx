fa68b8d66ebdda9ea27a50d902b031677b1bd44a
Cuong Nguyen <ctring23@gmail.com>
2018-04-20 22:17:32 -0400

    Scroll with fixed banner

--- genex-interface/react-app/App.jsx
+++ genex-interface/react-app/App.jsx
@@ -6,7 +6,7 @@ import { createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 
-import { Sidebar, Segment, Divider, Header, Grid } from 'semantic-ui-react'
+import { Sidebar, Segment, Divider, Header, Grid, SidebarPushable } from 'semantic-ui-react'
 
 import reducer from './reducers'
 import { HEADER_SIZE } from './constants'
@@ -19,15 +19,13 @@ import QuerySelectContainer from './containers/QuerySelectContainer'
 const Banner = () => (<div id='banner'>GENEX</div>);
 
 const App = () => {
-	let disableShadow = { 'boxShadow': '0 0 2px rgba(34,36,38,.15)' };
-	let controllerStyle = { 'padding': '10px' };
-	let resultAreaStyle = { 'padding': '15px' };
-	let sidebarFullHeight = { 'display': 'table-cell', 'height': 'inherit' };
+	let resultAreaStyle = { padding: '15px' };
+	let sidebarPushableStyle = { display: 'table-cell', 'height': 'inherit' };
 	return (
-		<Sidebar.Pushable style={sidebarFullHeight}>
-			<Sidebar animation='slide along' width='very wide' visible={true} style={disableShadow}>
+		<Sidebar.Pushable style={sidebarPushableStyle}>
+			<Sidebar animation='slide along' width='very wide' visible={true} id='sidebar'>
 				<Banner />
-				<div style={controllerStyle}>
+				<div id='controller'>
 					<Header as={HEADER_SIZE} icon='options' dividing content='Parameters' />
 					<DatasetSelectAndGroup />
 					<Divider />
