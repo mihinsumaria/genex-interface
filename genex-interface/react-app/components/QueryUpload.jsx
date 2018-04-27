e09072164588628f191576fed538517f3b1ce164
mihir <mihir9552@gmail.com>
2018-04-27 01:09:49 -0400

    resultvisualizationcontainer

---
+++ genex-interface/react-app/components/QueryUpload.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Tab } from 'semantic-ui-react'
+import PropTypes from 'prop-types'
+
+
+export default function QueryUpload() {
+    // let dataset = props.dataset;
+    return (
+        <div>
+            <h1>Something</h1>
+        </div>
+    )
+}
+
+// QueryFromDataset.propTypes = {};
