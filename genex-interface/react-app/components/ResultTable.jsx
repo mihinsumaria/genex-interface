86841c689c2b64fedb5a4be66864adfeba973af1
Mihin Sumaria <mihin.sumaria@gmail.com>
2018-09-15 21:45:46 -0400

    Title changes, added query, length, and rank to query table

--- genex-interface/react-app/components/ResultTable.jsx
+++ genex-interface/react-app/components/ResultTable.jsx
@@ -7,36 +7,50 @@ import 'fixed-data-table-2/dist/fixed-data-table.css';
 
 class ResultTable extends React.Component {
     render() {
-        const { width, result } = this.props;
-        let results = result.map((values) => (
+        const { result, query } = this.props;
+        let queryResult = {
+            name: query.name,
+            rank: 'Query',
+            start: query.start,
+            end: query.end,
+            length: query.end - query.start + 1,
+            distance: ''
+        };
+        let results = result.map((values, i) => (
             {
                 name: values.name,
-                index: values.data.index,
+                rank: i + 1,
                 start: values.data.start,
                 end: values.data.end,
+                length: values.data.end - values.data.start + 1,
                 distance: values.dist
             }
         ));
+        results.unshift(queryResult);
         let rowHeight = 50;
-        let height = results.length ? 
-            rowHeight * (results.length + 1.1) : 
-            Math.max(300, rowHeight * (results.length + 1));
+        let height = result.length ? Math.min(250, rowHeight * (results.length + 1)) : 250;
         return (
             <Table
                 rowHeight={rowHeight}
-                width={width}
+                width={800}
                 headerHeight={50}
                 height={height}
                 rowsCount={result.length} >
                 <Column
-                    header={<Cell>Name</Cell>}
+                    header={<Cell>Rank</Cell>}
+                    columnKey="rank"
+                    cell={<TextCell data={results} />}
+                    fixed={true}
+                    width={120} />
+                <Column
+                    header={<Cell>Timeseries</Cell>}
                     columnKey="name"
                     cell={<TextCell data={results} />}
                     fixed={true}
                     width={120} />
                 <Column
-                    columnKey="index"
-                    header={<Cell>Index</Cell>}
+                    columnKey="length"
+                    header={<Cell>Length</Cell>}
                     cell={<TextCell data={results} />}
                     fixed={true}
                     width={120} />
@@ -57,7 +71,7 @@ class ResultTable extends React.Component {
                     header={<Cell>Distance</Cell>}
                     cell={<TextCell data={results} />}
                     fixed={true}
-                    width={220} />
+                    width={200} />
             </Table>
         );
     }
@@ -66,6 +80,7 @@ class ResultTable extends React.Component {
 ResultTable.propTypes = {
     width: PropTypes.number,
     result: PropTypes.array,
+    query: PropTypes.object,
 };
 
 export default ResultTable;
